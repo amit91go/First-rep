181ab493346d721e0ab42eebb85d34b3007a6c86
Amit Yadav <amit91go@gmail.com>
2021-12-11 19:49:28 +0000

    added nw files for dcos

--- get_nw_data.sh
+++ get_nw_data.sh
@@ -17,7 +17,7 @@ do
                 do
                         rm -f nw_logs/ips nw_logs/begin nw_logs/diff nw_logs/pattern nw_logs/currentmatch nw_logs/ips_opp nw_logs/pattern_opp nw_logs/web_to_$1_tmp nw_logs/$1_to_web_tmp
                         echo $j|awk -F 'GET' '{print $2}' >>nw_logs/currentmatch
-                        grep -B3 `cat nw_logs/currentmatch` $i | grep length| awk -F 'IP|Flags' '{print $2}' >> nw_logs/ips
+                        grep -B4 `cat nw_logs/currentmatch` $i | grep length| tail -1| awk -F 'IP|Flags' '{print $2}' >> nw_logs/ips
                         sed -i 's/ /\\s/g' nw_logs/ips
                         cat nw_logs/ips >> nw_logs/pattern
                         sed -i 's/$/Flags\\s\\[F.\\]/g' nw_logs/ips
@@ -30,18 +30,23 @@ do
                         srcip=`tail -n +$begin $i| grep -m1 -e X-Forwarded-For -e Srcip|cut -f2 -d:`
 			echo $srcip
                         end=$(($begin + $diff))
-                        begin=$(($begin - 3))
+                        begin=$(($begin - 4))
                         sed -n "${begin},${end}p" $i | grep `cat nw_logs/pattern`| awk '{print $NF}' >> nw_logs/web_to_$1_tmp
                         cat nw_logs/web_to_$1_tmp|awk -v srcip="$srcip" '{ SUM += $1 } END { print srcip,":",SUM}' >> nw_logs/web_to_$1
                         echo $srcip
-                        tail -n +$begin $i| grep -B3 -m1 "200\sOK"| grep length | awk -F 'IP|Flags' '{print $2}' >> nw_logs/ips_opp
+                        tail -n +$begin $i| grep -B4 -m1 "200\sOK"| grep length |tail -1| awk -F 'IP|Flags' '{print $2}' >> nw_logs/ips_opp
+			echo $srcip
                         sed -i 's/ /\\s/g' nw_logs/ips_opp
                         cat nw_logs/ips_opp >> nw_logs/pattern_opp
                         sed -i 's/$/Flags\\s\\[F.\\]/g' nw_logs/ips_opp
                         failsafe_opp=`cat nw_logs/pattern_opp`"Flags\s\[(?!P)"
+			echo $srcip
                         begin_opp=`tail -n +$begin $i| grep -n -m1 "200\sOK"|cut -f1 -d:`
-                        begin_opp=$(($begin_opp + $begin - 4))
+			echo $srcip
+                        begin_opp=$(($begin_opp + $begin - 5))
+			echo $begin_opp
                         diff_opp=`tail -n +$begin_opp $i| grep -n -m1 \`cat nw_logs/ips_opp\`|cut -f1 -d:`
+			echo $diff_opp
                         if [ -z "$diff_opp" ]; then diff_opp=`tail -n +$begin_opp $i| grep -n -m1 -P $failsafe_opp|cut -f1 -d:`;fi
                         end_opp=$(($begin_opp + $diff_opp - 1))
                         sed -n "${begin_opp},${end_opp}p" $i | grep `cat nw_logs/pattern_opp`| awk '{print $NF}' >> nw_logs/$1_to_web_tmp
